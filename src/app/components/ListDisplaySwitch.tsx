ac01b7897399dc2957b9f95885b1e5714fdd40ca
gmrchk <gmarcuk@gmail.com>
2023-04-24 16:09:01 +0200

    active state

--- src/app/components/ListDisplaySwitch.tsx
+++ src/app/components/ListDisplaySwitch.tsx
@@ -3,6 +3,37 @@ import Image from "next/image";
 import list from "@/app/images/list.svg";
 import headlinedList from "@/app/images/headlined-list.svg";
 import React from "react";
+import { styled } from "@stitches/react";
+
+const StyledButton = styled(Button, {
+  position: "relative",
+
+  "&:before": {
+    content: "",
+    display: "block",
+    position: "absolute",
+    top: "3px",
+    left: "50%",
+    width: "4px",
+    height: "4px",
+    borderRadius: "50%",
+    background: "#000",
+    opacity: 0,
+    transform: "translate3d(-2px, -4px, 0)",
+    transition: ".2s",
+  },
+
+  variants: {
+    active: {
+      true: {
+        "&:before": {
+          opacity: 1,
+          transform: "translate3d(-2px, 0, 0)",
+        },
+      },
+    },
+  },
+});
 
 export const ListDisplaySwitch = (props: {
   onPress: (display: "list" | "headlinedList") => void;
@@ -11,36 +42,24 @@ export const ListDisplaySwitch = (props: {
   return (
     // @ts-ignore
     <Button.Group flat size={"sm"} color="neutral">
-      <Button onPress={() => props.onPress("list")}>
+      <StyledButton
+        onPress={() => props.onPress("list")}
+        active={props.listDisplay === "list"}
+      >
+        <Image src={list.src} alt="List" width={14} height={14} priority />
+      </StyledButton>
+      <StyledButton
+        onPress={() => props.onPress("headlinedList")}
+        active={props.listDisplay === "headlinedList"}
+      >
         <Image
-          style={{
-            filter:
-              props.listDisplay === "list"
-                ? "drop-shadow(0 0 2px 2px rgb(0 0 0 / 0))"
-                : "",
-          }}
-          src={list.src}
-          alt="List"
-          width={14}
-          height={14}
-          priority
-        />
-      </Button>
-      <Button onPress={() => props.onPress("headlinedList")}>
-        <Image
-          style={{
-            filter:
-              props.listDisplay === "headlinedList"
-                ? "drop-shadow(0 0 2px 2px rgb(0 0 0 / 0))"
-                : "",
-          }}
           src={headlinedList.src}
           alt="Headlined List"
           width={14}
           height={14}
           priority
         />
-      </Button>
+      </StyledButton>
     </Button.Group>
   );
 };
