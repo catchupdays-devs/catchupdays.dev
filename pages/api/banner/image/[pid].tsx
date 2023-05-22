2a81d950c9cc65f845d436dfb5bc8f2202a82cf7
gmrchk <gmarcuk@gmail.com>
2023-05-22 21:20:02 +0200

    banner

--- pages/api/banner/image/[pid].tsx
+++ pages/api/banner/image/[pid].tsx
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const filePath = path.resolve("./", "src/app/images/banner3-min.png");
+const filePath = path.resolve("./", "src/app/images/banner4-min.png");
 const imageBuffer = fs.readFileSync(filePath);
 
 export default function Image(req: NextApiRequest, res: NextApiResponse) {
