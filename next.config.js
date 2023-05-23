afe2aa33013847e744319baf717db7a6f4e5af0c
gmrchk <gmarcuk@gmail.com>
2023-05-23 13:57:14 +0200

    adjust CDN cache config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  async headers() {
+    return [
+      {
+        source: "/api/wishlist",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=86400, stale-while-revalidate=59",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
